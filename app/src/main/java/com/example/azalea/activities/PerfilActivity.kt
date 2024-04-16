b791ea904b9fbee9adfe9ba46e5b3be132ef6fed
bananna2 <anasofiarodriguez@MacBook-Air-de-Ana.local>
2024-04-15 23:02:54 -0500

    opcion de elegir foto de camera y gallery

--- app/src/main/java/com/example/azalea/activities/PerfilActivity.kt
+++ app/src/main/java/com/example/azalea/activities/PerfilActivity.kt
@@ -1,101 +1,144 @@
 package com.example.azalea.activities
 
+import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.media.MediaScannerConnection
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.azalea.R
 import com.example.azalea.data.PermissionsCodes.Companion.CAMERA_PERMISSION_CODE
 import com.example.azalea.databinding.ActivityPerfilBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class PerfilActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPerfilBinding
+    private lateinit var preferences: SharedPreferences
+    private lateinit var profileImage: ImageButton
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        profileImage.setImageURI(uri)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPerfilBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_perfil)
+        preferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
 
-        setUpButtonsWithoutPermissions()
-    }
+        // Cargar la imagen de perfil si existe
+        val imagePath = preferences.getString("imagePath", null)
 
-    private fun setUpButtonsWithoutPermissions() {
-        binding.profileImage.setOnClickListener {
-            checkPermissionForCamera(this)
+        profileImage = findViewById(R.id.profileImage)
+
+        if (imagePath != null) {
+            profileImage.setImageURI(Uri.parse(imagePath))
         }
 
-        binding.buttonEditProfile.setOnClickListener {
-            val intent = Intent(this, AddBasicDataActivity::class.java)
-            startActivity(intent)
+        profileImage.setOnClickListener {
+            showMediaOptions()
         }
+
     }
 
-    private fun setUpButtonsWithCameraPermissions() {
-        binding.profileImage.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivity(takePictureIntent)
-        }
-    }
-
-    private fun disableButtons() {
-        binding.profileImage.isEnabled = false
-    }
-
-    private fun checkPermissionForCamera(activity: AppCompatActivity) {
-        when{
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
-                // Permission already granted
-                setUpButtonsWithCameraPermissions()
+    private fun showMediaOptions() {
+        val options = arrayOf("Tomar foto", "Seleccionar de galería")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Selecciona una opción")
+        builder.setItems(options) { dialog, which ->
+            when (which) {
+                0 -> {
+                    if (checkCameraPermission()) {
+                        openCamera()
+                    } else {
+                        requestCameraPermission()
+                    }
+                }
+                1 -> pickMedia.launch("image/*")
             }
-
-            shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
-                // If user previously denied the permission
-                Toast.makeText(this, "Permission previously denied", Toast.LENGTH_SHORT).show()
-                requestPermission(this, android.Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE, "Needed for taking pictures")
-            }
-
-            else -> {
-                // Always call the own function to request permission, not the system one (requestPermissions)
-                requestPermission(this, android.Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE, "Needed for taking pictures")
-            }
-
         }
+        builder.show()
     }
 
-    private fun requestPermission(context: AppCompatActivity, permission: String, requestCode: Int, justify: String) {
-        if(ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-            if(shouldShowRequestPermissionRationale(permission)) {
-                Toast.makeText(this, justify, Toast.LENGTH_SHORT).show()
-            }
-            requestPermissions(arrayOf(permission), requestCode)
-        } else {
-            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
-        }
+    private fun checkCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_CODE)
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             CAMERA_PERMISSION_CODE -> {
-                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // Permission granted
-                    setUpButtonsWithCameraPermissions()
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
                 } else {
-                    // Permission denied
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                    disableButtons()
+                    Toast.makeText(this, "Permiso de cámara denegado.", Toast.LENGTH_SHORT).show()
                 }
-                return
-            }
-
-            else -> {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            saveImageToGallery(imageBitmap)
+            profileImage.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_SELECT_IMAGE && resultCode == RESULT_OK) {
+            val selectedImageUri: Uri? = data?.data
+            if (selectedImageUri != null) {
+                profileImage.setImageURI(selectedImageUri)
+                saveImageToGallery(selectedImageUri)
+            }
+        }
+    }
+
+    private fun saveImageToGallery(bitmap: Bitmap) {
+        val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val imageFile = File(imagesDir, "profile_image_${System.currentTimeMillis()}.jpg")
+        val outputStream = FileOutputStream(imageFile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        outputStream.flush()
+        outputStream.close()
+        MediaScannerConnection.scanFile(this, arrayOf(imageFile.absolutePath), null, null)
+
+        // Guardar la ruta de la imagen en SharedPreferences
+        val editor = preferences.edit()
+        editor.putString("imagePath", imageFile.absolutePath)
+        editor.apply()
+    }
+
+    private fun saveImageToGallery(imageUri: Uri) {
+        // Guardar la ruta de la imagen en SharedPreferences
+        val editor = preferences.edit()
+        editor.putString("imagePath", imageUri.toString())
+        editor.apply()
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+        private const val REQUEST_IMAGE_CAPTURE = 101
+        private const val REQUEST_SELECT_IMAGE = 102
     }
 }
