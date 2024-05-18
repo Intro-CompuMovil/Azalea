9c339fdae1cfefb0ef1d39e579f8f711edaf6dfa
czar <cesarandresolarte@gmail.com>
2024-05-17 22:03:10 -0500

    Adding contacts functionality fully implemented

--- app/src/main/java/com/example/azalea/activities/RegistrarContactoActivity.kt
+++ app/src/main/java/com/example/azalea/activities/RegistrarContactoActivity.kt
@@ -18,6 +18,7 @@ import com.example.azalea.adapters.EmergencyContactsAdapter
 import com.example.azalea.data.PermissionsCodes.Companion.CONTACTS_PERMISSION_CODE
 import com.example.azalea.data.User
 import com.example.azalea.databinding.ActivityRegistrarContactoBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -25,6 +26,8 @@ import com.google.firebase.database.ValueEventListener
 
 class RegistrarContactoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrarContactoBinding
+    private val userList = mutableMapOf<String, User>()
+    private var mAdapter: AddEmergencyContactsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,48 +35,123 @@ class RegistrarContactoActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         loadContacts()
-        //setUpSearch()
-        // TODO Add option to search for contacts with their emails (filter the users)
+        setUpSearchUI()
     }
 
     private fun loadContacts() {
         binding.recyclerViewAddContact.visibility = android.view.View.GONE
-
-        val listUsers = arrayListOf<User>()
-        val listUids = arrayListOf<String>()
         val databaseRef = FirebaseDatabase.getInstance().getReference("Users")
 
         databaseRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                listUsers.clear()
+                userList.clear()
                 if(!snapshot.exists()){
                     return
                 }
                 for (userSnapshot in snapshot.children) {
                     val user = userSnapshot.getValue(User::class.java)
                     user?.let {
-                        listUsers.add(it)
+                        if (userSnapshot.key.toString() != FirebaseAuth.getInstance().currentUser?.uid!!) {
+                            userList[userSnapshot.key.toString()] = it
+                        }
                     }
-                    listUids.add(userSnapshot.key.toString())
                 }
-                binding.recyclerViewAddContact.layoutManager = LinearLayoutManager(this@RegistrarContactoActivity)
-                val mAdapter = AddEmergencyContactsAdapter(listUsers, listUids)
-                binding.recyclerViewAddContact.adapter = mAdapter
-
-                mAdapter.setOnItemClickListener(object : AddEmergencyContactsAdapter.onItemClickListener {
-                    override fun onItemClick(position: Int) {
-                        // TODO Add contact to the emergency contacts list
-                        Toast.makeText(this@RegistrarContactoActivity, "Contact ${listUsers[position].name} added", Toast.LENGTH_SHORT).show()
-                    }
-                })
-
-                binding.recyclerViewAddContact.visibility = android.view.View.VISIBLE
+                removeCurrentContacts()
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.e("RegistrarContactoActivity", "Error loading contacts", error.toException())
             }
         })
+    }
 
+    private fun removeCurrentContacts() {
+        // Get users current contacts to avoid adding them again
+        val databaseRefCurrentContacts = FirebaseDatabase.getInstance().getReference("Users/${FirebaseAuth.getInstance().currentUser?.uid}")
+
+        databaseRefCurrentContacts.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val currentUser = snapshot.getValue(User::class.java)
+                if (currentUser != null) {
+                    for (contact in currentUser.emergencyContacts) {
+                        userList.remove(contact)
+                    }
+                    setUpRecyclerView()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("RegistrarContactoActivity", "Error loading current contacts", error.toException())
+            }
+        })
+    }
+
+    private fun setUpRecyclerView() {
+        binding.recyclerViewAddContact.layoutManager = LinearLayoutManager(this@RegistrarContactoActivity)
+        mAdapter = AddEmergencyContactsAdapter(userList)
+        binding.recyclerViewAddContact.adapter = mAdapter
+
+        mAdapter!!.setOnItemClickListener(object : AddEmergencyContactsAdapter.onItemClickListener {
+            override fun onItemClick(position: Int, userListAdapter: MutableMap<String, User>) {
+                // Add contact to the user's emergency contacts
+                val currentUID = FirebaseAuth.getInstance().currentUser?.uid
+                val contactUID = userListAdapter.keys.elementAt(position)
+                val databaseRef = FirebaseDatabase.getInstance().getReference("Users")
+
+                databaseRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if(!snapshot.exists()){
+                            return
+                        }
+                        for (userSnapshot in snapshot.children) {
+                            // If its current user then add emergency contact to the list of emergency contacts
+                            // If its the contact user then add current user to the list of emergency contact for
+                            if (userSnapshot.key.toString() == currentUID) {
+                                val currentUser = userSnapshot.getValue(User::class.java)
+                                if (currentUser != null) {
+                                    currentUser.emergencyContacts += contactUID
+                                    userSnapshot.ref.setValue(currentUser)
+                                }
+                            }else if (userSnapshot.key.toString() == contactUID) {
+                                val contactUser = userSnapshot.getValue(User::class.java)
+                                if (contactUser != null) {
+                                    contactUser.emergencyContactFor += currentUID!!
+                                    userSnapshot.ref.setValue(contactUser)
+                                }
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.e("RegistrarContactoActivity", "Error adding contact", error.toException())
+                    }
+                })
+
+                Toast.makeText(this@RegistrarContactoActivity, "Contact ${userList.values.elementAt(position).name} added", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.recyclerViewAddContact.visibility = android.view.View.VISIBLE
+    }
+
+    private fun setUpSearchUI() {
+        binding.editTextSearchContact.addTextChangedListener(object : android.text.TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchSimilarContacts(s.toString())
+            }
+
+            override fun afterTextChanged(s: android.text.Editable?) {
+                // Do nothing
+            }
+        })
+    }
+
+    private fun searchSimilarContacts(query: String) {
+        val filteredList = userList.filterValues { it.email.contains(query, ignoreCase = true) }
+        mAdapter?.updateList(filteredList.toMutableMap())
     }
 }
--- app/src/main/java/com/example/azalea/adapters/AddEmergencyContactsAdapter.kt
+++ app/src/main/java/com/example/azalea/adapters/AddEmergencyContactsAdapter.kt
@@ -1,29 +1,22 @@
 package com.example.azalea.adapters
 
-import android.content.Context
-import android.database.Cursor
-import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.Button
-import android.widget.CursorAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.azalea.R
 import com.example.azalea.data.User
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 
-class AddEmergencyContactsAdapter(private val userList: ArrayList<User>, private val uidList: ArrayList<String>) : RecyclerView.Adapter<AddEmergencyContactsAdapter.ViewHolder>() {
+class AddEmergencyContactsAdapter(private var userListAdapter: MutableMap<String, User>) : RecyclerView.Adapter<AddEmergencyContactsAdapter.ViewHolder>() {
     private lateinit var mListener: onItemClickListener
 
     interface onItemClickListener{
-        fun onItemClick(position: Int)
+        fun onItemClick(position: Int, userListAdapter: MutableMap<String, User>)
     }
 
     fun setOnItemClickListener(clickListener: onItemClickListener){
@@ -32,12 +25,12 @@ class AddEmergencyContactsAdapter(private val userList: ArrayList<User>, private
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_add_emergency_contacts, parent, false)
-        return ViewHolder(view, mListener)
+        return ViewHolder(view, mListener, this)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Get information from different users
-        val currentUser = userList[position]
+        val currentUser = userListAdapter.values.elementAt(position)
         holder.nameContactEmergency.text = currentUser.name
         holder.emailContactEmergency.text = currentUser.email
         holder.availabilityContactEmergency.setImageResource(if (currentUser.available) android.R.drawable.presence_online else android.R.drawable.presence_offline)
@@ -45,21 +38,31 @@ class AddEmergencyContactsAdapter(private val userList: ArrayList<User>, private
         // Load image from Firebase Storage
         val storage = FirebaseStorage.getInstance()
         val storageRef = storage.reference
-        val imageRef = storageRef.child("pfp/${uidList[position]}")
-        val localFile = File.createTempFile("pfp${uidList[position]}", "jpg")
+        val imageRef = storageRef.child("pfp/${userListAdapter.keys.elementAt(position)}")
+        val localFile = File.createTempFile("pfp${userListAdapter.keys.elementAt(position)}", "jpg")
         imageRef.getFile(localFile).addOnSuccessListener {
             val bitmap = android.graphics.BitmapFactory.decodeFile(localFile.absolutePath)
             holder.imageContactEmergency.setImageBitmap(bitmap)
         }.addOnFailureListener {
-            Log.d("AddEmergencyContactsAdapter", "Failed to load image")
+            holder.imageContactEmergency.setImageResource(R.drawable.pfp)
         }
     }
 
     override fun getItemCount(): Int {
-        return userList.size
+        return userListAdapter.size
     }
 
-    class ViewHolder(itemView: View, clickListener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
+    fun removeItem(position: Int) {
+        userListAdapter -= userListAdapter.keys.elementAt(position)
+        notifyItemRemoved(position)
+    }
+
+    fun updateList(newList: MutableMap<String, User>) {
+        userListAdapter = newList
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View, clickListener: onItemClickListener, private val adapter: AddEmergencyContactsAdapter) : RecyclerView.ViewHolder(itemView) {
         val nameContactEmergency: TextView = itemView.findViewById(R.id.nameAddContactEmergency)
         val emailContactEmergency: TextView = itemView.findViewById(R.id.emailAddContactEmergency)
         val imageContactEmergency: ImageView = itemView.findViewById(R.id.imgAddContact)
@@ -69,7 +72,9 @@ class AddEmergencyContactsAdapter(private val userList: ArrayList<User>, private
 
         init {
             addContactButton.setOnClickListener {
-                clickListener.onItemClick(adapterPosition)
+                clickListener.onItemClick(adapterPosition, adapter.userListAdapter)
+                // Remove itself from the list
+                adapter.removeItem(adapterPosition)
             }
         }
     }
